5db19d495a20327281de9a11a26a713ca58c19ef
CodeForFun7325 <johnnguyen7325@outlook.com>
2025-03-31 21:48:26 -0700

    Add introboard
    Add introboard

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import IntroBoard from './components/introduction/introboard';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <IntroBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/introduction/introboard.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import './introboard.css'; 
+
+export default function IntroBoard() { 
+  return (
+    <div className="intro-board-container"> 
+        <h1 className="my-name">John Nguyen</h1> 
+        <h2 className="job-title">Software Developer</h2>
+
+        <p className="about-me">
+          As a software developer I enjoy turning complex problems into simple, beautiful, and intuitive designs. When I'm not coding, you can find me exploring new technologies or enjoying a good cup of coffee.
+        </p>
+    </div>
+  );
+}
